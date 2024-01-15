56389d598ff556a3fd9b62c7bbaa7491359971e1
adnankhayrou <adnanekhayrou3@gmail.com>
2024-01-15 14:58:38 +0100

    adding update category with id function

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -31,10 +31,30 @@ const getCategoryWithId = async (req, res) => {
     }
 };
 
+const updateCategory = async (req, res) => {
+    const {error} = categoryValidation(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    
+    try {
+        const { id } = req.params;
+        const updatedCategory = await Category.findByIdAndUpdate(id, req.body);
+        if (!updatedCategory) {
+            return res.status(404).json({ error: "Category not found" });
+        }
+        res.json({success: "Category updated successfully", updatedCategory});
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({ error: "Something went wrong" });
+    }
+};
+
 
 
 module.exports = {
     createNewCategory,
     getCategoryWithId,
+    updateCategory,
     
 };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,6 +5,7 @@ const categoryController = require('../controllers/categoryController');
 
 router.post('/createCategory', categoryController.createNewCategory);
 router.get('/getCategory/:id', categoryController.getCategoryWithId);
+router.post('/updateCategory/:id', categoryController.updateCategory);
 
 
 module.exports = router;
